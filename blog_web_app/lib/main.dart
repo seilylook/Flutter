367ca29cee26841a62f9d89b28dd0fd576b34048
seilylook <seilylook@naver.com>
2023-04-18 11:17:31 +0900

    FEAT:flutter projects repository implement

---
+++ blog_web_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:blog_web_app/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
+
---
+++ image_carousel/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:image_carousel/screen/home_scree.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
